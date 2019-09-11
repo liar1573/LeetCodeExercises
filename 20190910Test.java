cf839d8c9161cee62ef65b14f9fab141bd366ddb
liar1573 <34848171+liar1573@users.noreply.github.com>
2019-09-11 11:08:43 +0800

    成对反转链表中节点

--- 20190910Test.java
+++ 20190910Test.java
@@ -31,26 +31,56 @@ public class Test {
 	}
 	
 	
-	public static ListNode swapPairs(ListNode head) {
+		public static ListNode swapPairs(ListNode head) {
+
 		if ((null == head) || (null == head.next)) {
+
 			return head;
+
 		}
 		
-		
-
 		ListNode pre,last,p1,p2;
 
 		pre = head;
+
 		p1 = pre;p2 = pre.next;
+
 		last = p2.next;
+
+		//突然发现好像head没有更新过，一直是指向第一次传进来的头节点，这样有点问题。。
+		//在length >= 2的情况下，p1和p2是肯定要交换一次的，先手动把head设置为p2好了
+		head = p2;
+			
 		do{
+
 		p1.next = last;
+
 		pre = p2; p2.next = p1;
-		}while((last != null) && (last.next != null));
 		
+		//指针移动到下一组节点进行调换
+		//if((last != null) && (last.next != null))
+		if((last == null) || (last.next == null))
+			break;//这里判断了之后while的判断应该可以去掉了
+		//这里if的逻辑应该是while逻辑的取反，一开始想错了
+		
+		pre = last; p1 = last; p2 = p1.next;last = p2.next;
+		
+		
+		//组内节点的调换，组间节点的连接好像还是有些问题，后面再思考一下
+		
+
+//		}while((last != null) && (last.next != null));
+		}while(true);
+		//第一遍调试进行了一次do就跳出了？应该是两次啊？？
+		
+//第一遍调试，1234被换成了134？？2哪去了？？
+		
+
 		return head;
+
     }
-	
+		
+			
 	static void printLinkedList(ListNode head){
 		while ((null != head)) {
 			System.out.print(head.val + "==>");
